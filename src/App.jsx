ae641f867b8396a847075b416056213338d8e008
thiendao <thiendaocong@gmail.com>
2024-10-04 17:18:27 +0700

    thin

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-     <h1 className="text-3xl font-bold underline bg-red-600">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline bg-red-600">thien</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
