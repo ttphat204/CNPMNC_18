5af8cfa3c8455898b386e228ed81c721b3d55c0c
vothanhtung96 <vothanhtung056@gmail.com>
2024-10-04 16:11:36 +0700

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ function App() {
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline bg-red-600">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline bg-red-600">
+        Test 1
+      </h1>
     </>
   )
 }
